011c777354d426f7622439c8a860fb8d09d10731
Jino <jinomanoj3@gmail.com>
2024-01-02 16:38:54 +0530

    updated db with data

--- db/egarden.sql
+++ db/egarden.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.7
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 12, 2023 at 02:19 AM
--- Server version: 5.7.41-cll-lve
--- PHP Version: 7.4.30
+-- Generation Time: Jan 02, 2024 at 03:52 AM
+-- Server version: 5.7.44-cll-lve
+-- PHP Version: 8.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 

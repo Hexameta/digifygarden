63a718e47169801bd1b69bf44c556ce603f81b73
mhdyaseen8841 <riyaskh123@gmail.com>
2024-02-13 08:58:05 +0530

    log date change

--- College/tree-details.php
+++ College/tree-details.php
@@ -175,7 +175,8 @@ if ($offerResult) {
 
 
   if (!isset($_SESSION['u_id'])) {
-    mysqli_query($conn, "insert into tbl_scan_log(tree_id,ip_address) values('$t_id','" . get_client_ip() . "')");
+    $curTime = date('Y-m-d H:i:s');
+    mysqli_query($conn, "insert into tbl_scan_log(tree_id,ip_address,time) values('$t_id','" . get_client_ip() . "','$curTime')");
   }
 
 

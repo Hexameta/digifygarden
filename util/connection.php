69f579931be6c8c97135bc558cba6a8b4ec81a21
riyaskh123 <riyaskh123@gmail.com>
2025-03-07 10:04:36 +0400

    git push

--- util/connection.php
+++ util/connection.php
@@ -1,15 +1,15 @@
 <?php
 //live host
-// $servername = "localhost";
-// $username = "digifyadmin";
-// $password = "Digify@alameen2023";
-// $dbname = "egarden";
+$servername = "localhost";
+$username = "digifyadmin";
+$password = "Digify@alameen2023";
+$dbname = "egarden";
 
 // local
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "egarden";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "egarden";
 
 // live server
 // $servername = "68.178.145.241";
